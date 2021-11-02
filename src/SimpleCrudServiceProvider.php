665a6932feaabe92db34efaa63a625150e6063d7
sajjadgozalzadeh <sajjad.gozalzadeh@picasso.com>
2021-11-02 17:11:50 +0300

    refactor view folders

--- src/SimpleCrudServiceProvider.php
+++ src/SimpleCrudServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace sajjadgozal\SimpleCrud;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class SimpleCrudServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadViewsFrom(__DIR__.'/views', 'SimpleCrud');
+
         $this->publishes([
             __DIR__.'/../resources/' => base_path('resources/'),
             __DIR__.'/../config/crud.php' => config_path('crud.php')
