803b5c27eee264b735d0a1660aabe44220bed8a5
sajjadgozalzadeh <sajjad.gozalzadeh@picasso.com>
2021-11-02 13:56:04 +0300

    config file for publish

--- src/SimpleCrudServiceProvider.php
+++ src/SimpleCrudServiceProvider.php
@@ -4,13 +4,14 @@ namespace sajjadgozal\ServiceProviders;
 
 use Illuminate\Support\ServiceProvider;
 
-class SimpleCrudProvider extends ServiceProvider
+class SimpleCrudServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/views', 'todolist');
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/Crud'),
+            __DIR__.'/../resources/' => base_path('resources/'),
+            __DIR__.'/../config/crud.php' => config_path('crud.php'),
         ]);
     }
 }
